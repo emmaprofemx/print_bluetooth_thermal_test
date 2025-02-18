d718f1c139e2b85708ae0951a1f3eac1aa465b22
emmaprofemx <emmaprofemx@gmail.com>
2025-02-18 01:30:46 -0600

    Encabezado ticket
    Realizando el encabezado de un ticket de muestra

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -366,17 +366,20 @@ class MyAppState extends State<MyApp> {
       // Resizes the image to half its original size and reduces the quality to 80%
       final resizedImage = img.copyResize(image!, width: image.width ~/ 1.3, height: image.height ~/ 1.3, interpolation: img.Interpolation.nearest);
       final bytesimg = Uint8List.fromList(img.encodeJpg(resizedImage));
-      //image = img.decodeImage(bytesimg);
+      image = img.decodeImage(bytesimg);
     }
 
     //Using `ESC *`
-    //bytes += generator.image(image!);
+    bytes += generator.image(image!);
 
    // bytes += generator.text('Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
+    bytes += generator.text('IMPORTADORES UNIDOS MEXICANOS', styles: const PosStyles(codeTable: 'CP1252'));
+     bytes += generator.text('TAMAULIPAS #404, ZONA CENTRO', styles: const PosStyles(codeTable: 'CP1252'));
     bytes += generator.text('================================', styles: const PosStyles(codeTable: 'CP1252'));
-    bytes += generator.text('CANT DESCRIPCION', styles: const PosStyles(codeTable: 'CP1252'));
-    bytes += generator.text('Special 1: ñÑ àÀ èÈ éÉ üÜ çÇ ôÔ', styles: const PosStyles(codeTable: 'CP1252'));
-    bytes += generator.text('Special 2: blåbærgrød', styles: const PosStyles(codeTable: 'CP1252'));
+    bytes += generator.text('CANT DESCRIPCION         IMPORTE', styles: const PosStyles(codeTable: 'CP1252'));
+    bytes += generator.text('================================', styles: const PosStyles(codeTable: 'CP1252'));
+    //bytes += generator.text('Special 1: ñÑ àÀ èÈ éÉ üÜ çÇ ôÔ', styles: const PosStyles(codeTable: 'CP1252'));
+    //bytes += generator.text('Special 2: blåbærgrød', styles: const PosStyles(codeTable: 'CP1252'));
 
     bytes += generator.text('Bold text', styles: const PosStyles(bold: true));
     bytes += generator.text('Reverse text', styles: const PosStyles(reverse: true));
